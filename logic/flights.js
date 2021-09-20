ac5c47fcf81faa1873ffc9f221befa2fd91b1365
Chad O'Brien <obrien.music@gmail.com>
2021-09-19 23:52:52 -0400

    fixed bug that wasn't mentioned in the instructions at all...

--- logic/flights.js
+++ logic/flights.js
@@ -1,7 +1,7 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity) {
-        if (passengers < 0) {throw new Error('The number of passengers must be a positive integer value')};
-        if (capacity < 0) {throw new Error('The capacity of the flight must be a positive integer value')};
+        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {throw new Error('The number of passengers must be a positive integer value')};
+        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {throw new Error('The capacity of the flight must be a positive integer value')};
         return Math.ceil(passengers/capacity);
     }
     return {calculateNumberOfFlights};
