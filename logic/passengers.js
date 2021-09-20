d73576e4b149eac0e33607004f8c4206bc7e5974
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 08:20:53 -0400

    copy their stupid version

--- logic/passengers.js
+++ logic/passengers.js
@@ -8,43 +8,60 @@ function Passengers() {
         }
     }
 
-    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, flights, businessSeatsPerFlight, economySeatsPerFlight) {
-        var seatingObject = {
-            businessVips: 0,
-            economyVips: 0,
-            businessRegs: 0,
-            economyRegs: 0
-        }
+    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights, 
+        businessSeatsPerFlight, economySeatsPerFlight) {
 
-        let totalBusinessSeats = flights * businessSeatsPerFlight;
-        let totalEconomySeats = flights * economySeatsPerFlight;
-        
-        if (totalBusinessSeats >= vipPassengers) { //there are exactly enough or more than enough business seats for VIPs
-            seatingObject.businessVips = vipPassengers;
-            totalBusinessSeats -= vipPassengers;
-            if (totalBusinessSeats >= regularPassengers) { //enough or more than enough business seats for regs
-                seatingObject.businessRegs = regularPassengers;
-            } else { //not enough business seats for regs
-                seatingObject.businessRegs = totalBusinessSeats;
-                regularPassengers -= totalBusinessSeats;
-                seatingObject.economyRegs = Math.min(totalEconomySeats, regularPassengers);
-            }
-        } else { //not enough business seats for VIPs
-            seatingObject.businessVips = totalBusinessSeats;
-            vipPassengers -= totalBusinessSeats;
-            if (vipPassengers >= totalEconomySeats) { //more VIPs than economy seats or exactly the amount
-                seatingObject.economyVips = totalEconomySeats;
-            } else { //more economy seats than VIPs
-                seatingObject.economyVips = vipPassengers;
-                totalEconomySeats -= vipPassengers;
-                seatingObject.economyRegs = Math.min(totalEconomySeats, regularPassengers);
-            }
-         }
+        let vipPassengersWithBusinessSeats=0, vipPassengersWithEconomySeats=0, 
+        regularPassengersWithBusinessSeats=0, regularPassengersWithEconomySeats=0;
+        let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
+        let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
 
-        return seatingObject;
+        var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
+        vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
+
+        var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers, seats:availableEconomySeats};
+        vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
+
+        var regularBusinessConfiguration = {passengers:regularPassengers, seats:vipBusinessConfiguration.seats};
+        regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
+
+        var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers, seats:vipEconomyConfiguration.seats};
+        regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
+
+        return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats,
+                vipPassengersWithEconomySeats:vipPassengersWithEconomySeats, regularPassengersWithBusinessSeats:regularPassengersWithBusinessSeats,
+                regularPassengersWithEconomySeats:regularPassengersWithEconomySeats};
     }
 
-    return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
+    function updateConfiguration(configuration, seatsPerFlight) {
+        let passengersWithSeats = 0;
+        while (configuration.passengers > 0) {
+            if (configuration.seats > 0) {
+               if (configuration.passengers >= configuration.seats) {
+
+                    if (configuration.seats > configuration.seatsPerFlight) {
+                        configuration.passengers -= seatsPerFlight;
+                        configuration.seats -= seatsPerFlight;
+                        passengersWithSeats += seatsPerFlight;
+                    } else {
+                        configuration.passengers -= configuration.seats;
+                        passengersWithSeats += configuration.seats;
+                        configuration.seats = 0;
+                    }
+               } else {
+                    passengersWithSeats += configuration.passengers;
+                    configuration.seats -= configuration.passengers;
+                    configuration.passengers = 0;
+               }
+            } else {
+               break;
+            }
+        }
+        return passengersWithSeats;
+    }
+
+     return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
+
 }
 
 module.exports = Passengers();
