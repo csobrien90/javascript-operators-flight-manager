60e3cdf508c83ae036739a5c8b1976182aac23de
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 14:51:25 -0400

    fix typo

--- logic/price.js
+++ logic/price.js
@@ -1,12 +1,12 @@
 function Prices() {
-    function calculatedFinalPrice(basePrice, variationPassengerType, variationFlightType) {
+    function calculateFinalPrice(basePrice, variationPassengerType, variationFlightType) {
         let finalPrice = basePrice;
         finalPrice *= (100 + variationPassengerType) / 100;
         finalPrice *= (100 + variationFlightType) / 100;
 
         return finalPrice.toFixed(2);
     }
-    return {calculatedFinalPrice};
+    return {calculateFinalPrice};
 }
 
 module.exports = Prices();
