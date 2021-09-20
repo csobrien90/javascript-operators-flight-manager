60f452d0196bbbce136fdaffa948f038e8d19ee9
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 00:01:22 -0400

    step three for debugging

--- logic/flights.js
+++ logic/flights.js
@@ -4,7 +4,22 @@ function Flights() {
         if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {throw new Error('The capacity of the flight must be a positive integer value')};
         return Math.ceil(passengers/capacity);
     }
-    return {calculateNumberOfFlights};
+
+    function checkAircraftRevision(distanceLimit, distancesArray) {
+        let totalDistance = distancesArray.reduce((a, b) => a + b);
+        if (totalDistance <= distanceLimit / 2) {
+            return 'The revision needs to be done within the next 3 months';
+        } else if (totalDistance > distanceLimit / 2 && totalDistance <= distanceLimit * .75) {
+            'The revision needs to be done within the next 2 months';
+        } else if (totalDistance > distanceLimit * .75 && totalDistance <= distanceLimit) {
+            'The revision needs to be done within the next month';
+        } else {
+            throw new Error('Total distance cannot exceed distance limit');
+        }
+    }
+
+
+    return {calculateNumberOfFlights, checkAircraftRevision};
 }
 
 module.exports = Flights();
