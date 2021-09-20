a98cee6027a79610a75e92f925b589cca690c214
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 14:25:14 -0400

    complete module 1

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,19 @@
 function Util() {
+    function calculateTotalDistributedPassengers(seatingObject) {
+        let total = 0;
 
+        for (item in seatingObject) {total += seatingObject[item]};
+
+        return total;
+    }
+
+    function calculateTotalNumberOfPassengers(arr) {
+        return arr.reduce((a, b) => a + b);
+    }
+
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 }
+
+module.exports = Util();
 
