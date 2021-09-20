d5ae545cf04fb2057dcf04d8bbfc189c3ba65d78
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 14:50:43 -0400

    complete first step of module 2

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,12 @@
 function Prices() {
+    function calculatedFinalPrice(basePrice, variationPassengerType, variationFlightType) {
+        let finalPrice = basePrice;
+        finalPrice *= (100 + variationPassengerType) / 100;
+        finalPrice *= (100 + variationFlightType) / 100;
 
+        return finalPrice.toFixed(2);
+    }
+    return {calculatedFinalPrice};
 }
 
+module.exports = Prices();
