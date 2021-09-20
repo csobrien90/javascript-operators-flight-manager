7068af2d50a21b031b527bc7db8d4a29bf1a7b7a
Chad O'Brien <obrien.music@gmail.com>
2021-09-20 00:21:49 -0400

    step four for debugging

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,13 @@
 function Passengers() {
-
+    function checkFlightCapactiy(flightCapacity, passengersArray) {
+        let totalPassengers = passengersArray.reduce((a, b) => a + b);
+        if (totalPassengers <= flightCapacity) {
+            return totalPassengers;
+        } else {
+            throw new Error('Total passengers cannot exceed flight capacity');
+        }
+    }
+    return {checkFlightCapactiy};
 }
 
+module.exports = Passengers();
